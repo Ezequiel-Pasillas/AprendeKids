df962c080d08fb0e76a90fd37c4250c89ad07eb1
Ezequiel Pasillas Gonzalez <129228529+Ezequiel-Pasillas@users.noreply.github.com>
2023-05-02 23:32:54 -0600

    Delete lector.js

--- lector.js
+++
@@ -1,7 +0,0 @@
-function leerTexto(texto) {
-    //sacar valor de un texto
-    //var texto = document.getElementById("titulo_texto").textContent;
-    var mensaje = new SpeechSynthesisUtterance(texto);
-    var sintesisDeVoz = window.speechSynthesis;
-    sintesisDeVoz.speak(mensaje);
-    }
