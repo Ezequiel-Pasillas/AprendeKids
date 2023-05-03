c6d2248d73ba09df5e2bf113afb1ea4778c919aa
Haxis0911 <isaac.tovar7786@alumnos.udg.mx>
2023-05-03 11:32:43 -0600

    Mejora de lector

--- About-Isaac/Js/lector.js
+++ About-Isaac/Js/lector.js
@@ -1,7 +1,26 @@
+//funcion para ller texto dividiendolo por partes
 function leerTexto(texto) {
-    //sacar valor de un texto
-    //var texto = document.getElementById("titulo_texto").textContent;
-    var mensaje = new SpeechSynthesisUtterance(texto);
-    var sintesisDeVoz = window.speechSynthesis;
-    sintesisDeVoz.speak(mensaje);
+    play.textContent = "Repetir";
+    window.speechSynthesis.cancel();
+    var partes = texto.split(".");
+    var parteActual = 0;
+    var parteTotal = partes.length;
+    var mensaje = new SpeechSynthesisUtterance();
+    
+    mensaje.onend = function() {
+      parteActual++;
+      if (parteActual < parteTotal) {
+        mensaje.text = partes[parteActual].trim() + ".";
+        window.speechSynthesis.speak(mensaje);
+      }
     }
+  
+    mensaje.text = partes[parteActual].trim() + ".";
+    window.speechSynthesis.speak(mensaje);
+  }
+
+
+  //callarlo cuando salga
+  window.addEventListener("beforeunload", function(event) {
+  window.speechSynthesis.cancel();
+});
