75c92849ecb98e5ecb39a96699803bf18d96e42b
Ezequiel Pasillas Gonzalez <129228529+Ezequiel-Pasillas@users.noreply.github.com>
2023-05-02 23:33:58 -0600

    Add files via upload

---
+++ About-Isaac/Js/lector.js
@@ -0,0 +1,7 @@
+function leerTexto(texto) {
+    //sacar valor de un texto
+    //var texto = document.getElementById("titulo_texto").textContent;
+    var mensaje = new SpeechSynthesisUtterance(texto);
+    var sintesisDeVoz = window.speechSynthesis;
+    sintesisDeVoz.speak(mensaje);
+    }
