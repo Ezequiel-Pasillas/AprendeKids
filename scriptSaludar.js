85de9409b20fd4325867e1759395213ba30c5153
Ezequiel Pasillas Gonzalez <129228529+Ezequiel-Pasillas@users.noreply.github.com>
2023-05-02 23:33:15 -0600

    Delete scriptSaludar.js

--- scriptSaludar.js
+++
@@ -1,9 +0,0 @@
-const module = require('./script');
-
-function escribe(){    
-        var etiquetaP = document.getElementById("prueba");
-        etiquetaP.innerHTML = archivo1.nombreGuardado;
-}
-// ejecución de la función cuando se carga la página
-escribe();
-    
