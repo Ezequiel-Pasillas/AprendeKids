e573fbf3ce6789aef7cc8514dc09921805a9bbea
Ezequiel Pasillas Gonzalez <129228529+Ezequiel-Pasillas@users.noreply.github.com>
2023-05-03 12:45:38 -0600

    Actualizacion del header a los archivos: index, vamosAprender, bienvenida

---
+++ Home-Ezequiel/Js/scriptSaludar.js
@@ -0,0 +1,9 @@
+const module = require('./script');
+
+function escribe(){    
+        var etiquetaP = document.getElementById("prueba");
+        etiquetaP.innerHTML = archivo1.nombreGuardado;
+}
+// ejecución de la función cuando se carga la página
+escribe();
+    
